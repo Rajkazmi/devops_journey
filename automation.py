81dfa8c3cf357f37d8243508209f25b18789cc1f
raj <rajkazmi.era@gmail.com>
2026-07-20 05:12:51 +0000

    added my first python automation script

---
+++ automation.py
@@ -0,0 +1,6 @@
+print("-----------------------------------------")
+print("Welcome to MY Devops Automation Programme")
+print("-----------------------------------------")
+
+for i in range(1, 6):
+    print(f"server Instance Number {i} is starting...")
